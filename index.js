ad2d9dfe1105d8c3bc1e466865f461387f4eaa3f
abattalov <abattalov14@gmail.com>
2022-07-04 16:45:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions here
+function map(src, callback){
+    const newArray = [];
+
+    for (let i = 0; i < src.length; i++){
+        const newValue = src[i];
+        newArray.push(callback(newValue))
+    }
+
+    return newArray
+}
+
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    }
+  }
